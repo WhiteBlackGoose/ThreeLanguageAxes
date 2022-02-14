bc57114ccbb1e59bba1d70b2a2ce2cd0b46a33ab
WhiteBlackGoose <wbg@angouri.org>
2022-02-14 12:10:21 +0300

    Update main.fsx

--- main.fsx
+++ main.fsx
@@ -23,7 +23,7 @@ let langs = [
     { name = "C++";        typing = typesafe 0.55; runtime = closeToHw 0.90; paradigm = declarative 0.15 }
     { name = "F#";         typing = typesafe 0.80; runtime = closeToHw 0.70; paradigm = declarative 0.80 }
     { name = "Python";     typing = typesafe 0.30; runtime = closeToHw 0.20; paradigm = declarative 0.40 }
-    { name = "Asm";        typing = typesafe 0.00; runtime = closeToHw 1.00; paradigm = declarative 0.00 }
+    { name = "Asm";        typing = typesafe 0.00; runtime = closeToHw 1.00; paradigm = declarative 0.05 }
     { name = "F*";         typing = typesafe 1.00; runtime = closeToHw 0.10; paradigm = declarative 1.00 }
     { name = "Javascript"; typing = typesafe 0.20; runtime = closeToHw 0.30; paradigm = declarative 0.40 }
     { name = "Kotlin";     typing = typesafe 0.80; runtime = closeToHw 0.50; paradigm = declarative 0.60 }
@@ -31,6 +31,7 @@ let langs = [
     { name = "Haskell";    typing = typesafe 0.90; runtime = closeToHw 0.80; paradigm = declarative 0.90 }
     { name = "Fresh";      typing = typesafe 0.75; runtime = closeToHw 0.70; paradigm = declarative 0.70 }
     { name = "Brainfuck";  typing = typesafe 0.00; runtime = closeToHw 0.95; paradigm = declarative 0.00 }
+    { name = "OCaml";      typing = typesafe 0.80; runtime = closeToHw 0.75; paradigm = declarative 0.80 }
 ]
 
 let src = html [] [
